0ce65641f9b69ed4b65299640d52b080ef330f37
Micu Dulnoan <micu.dulnoan@blackrock.com>
2019-03-16 14:37:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
@@ -7,3 +8,14 @@ var greetSpecialGuest = "Hello, " + specialGuest + "!";
 var topic = "space";
 
 var conversation = "Let's talk about " + `${topic}` + ".";
+=======
+var greeting = "!";
+
+var specialGuest = "Neil deGrasse Tyson"
+
+var greetSpecialGuest = greeting + specialGuest + "!";
+
+var topic = "space";
+
+var conversation = `${topic}`;
+>>>>>>> cca4df3e91a82d12138c32dff7e5c39af543e492
